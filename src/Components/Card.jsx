7b1df1ea92bb9ef3288ef8e0d8c1886918c61ce5
Jennifer <jennespinozavidal@gmail.com>
2023-06-27 00:40:28 -0400

    agregado Card

---
+++ src/Components/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Card = ({ personaje }) => {
+    return (
+        <section className='personaje'>
+            <div className='personaje-body'>
+                <figure>
+                    <img src={personaje.image} alt={personaje.name} />
+                </figure>
+
+                <h2>{personaje.name}</h2>
+                <p>
+                    {personaje.species} <span>-</span> {personaje.gender}
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Card
