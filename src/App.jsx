4ef055859e3c7990b28574979a5150e2c040313b
Jennifer <jennespinozavidal@gmail.com>
2023-06-26 22:45:12 -0400

    integrar api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import Api from './MiApi'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css'
 
+
 function App() {
-  
+  const [characters, setCharacters] = useState([]);
+
+
+  const getApi = async () => {
+    try {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+
+      console.log(data.results);
+
+      setCharacters(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getApi();
+  }, []);
 
   return (
-    <>
-    <Api></Api>
-    </>
+    <div className="App">
+      <h1>Hola</h1>
+      {
+        characters.map((characters) =>
+          <div key={characters.id}>
+            <h3>{characters.name}</h3>
+            <img src={characters.image} alt="" />
+            <p>{characters.species}</p>
+          </div>)
+      }
+    </div>
   )
 }
 
